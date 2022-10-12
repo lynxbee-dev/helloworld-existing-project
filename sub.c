844deb926ae8c83338d2fbc4da3dec0d2c867b3a
Lynxbee <lynxbee.dev@gmail.com>
2022-10-12 05:44:35 +0530

    Added Subtraction program
    Signed-off-by: Lynxbee <lynxbee.dev@gmail.com>

---
+++ sub.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	int subtraction, a = 10, b = 25;
+	subtraction = b - a;
+	printf("Subtraction of two numbers is: %d\n", subtraction);
+	return 0;
+}
